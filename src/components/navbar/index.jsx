658e3251431c2dd3a0e1010f78bf2a2aba32a9fb
Emanuel Martinez <yinproducciones@gmail.com>
2022-09-18 21:50:37 -0300

    agregando navbar

---
+++ src/components/navbar/index.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export const NavBar = () => {
+    return (
+        <div className="container">
+            <nav className="nav">
+                <div className="nav_brand">
+                    <a className="nav_link" href="#">Logo</a>
+                </div>
+                <ul className="nav_list">
+                    <li><a className="nav_link" href="#">Categoria 1</a></li>
+                    <li><a className="nav_link" href="#">Categoria 2</a></li>
+                    <li><a className="nav_link" href="#">Carrito</a></li>
+                </ul>
+            </nav>
+        </div>
+    );
+}
+
+export default NavBar;
